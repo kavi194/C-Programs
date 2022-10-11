2c8083bfd94be6c09167e457696e6418d90ae4a6
Kaviarasi I <82664182+kavi194@users.noreply.github.com>
2022-10-11 19:35:08 +0530

    Program for ascii value.

---
+++ Ascii.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char alpha;
+    printf("Enter letter in the alphabet:");
+    scanf("%c",&alpha);
+    printf("\n The ascii value of alphabet variable is:%d",alpha);
+    return 0;
+}
